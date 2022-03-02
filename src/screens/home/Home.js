394a96c1591632bf306355fba858cafbd9b56c7d
rfinnegan <81394236+rfinnegan@users.noreply.github.com>
2022-03-02 14:27:33 -0500

    lorem ipsum

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -40,7 +40,7 @@ const Home = () => {
           <div class="blurb-container">Cutting edge decision making in today's financial markets</div>
           <div class="paragraph-container">
             <p>
-              information about the company.information about the company. information about the company.information about the company. information about the company.information about the company.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
           </div>
         </div>
