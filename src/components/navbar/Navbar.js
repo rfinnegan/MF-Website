3c9aaf58c394dffa1e3fb2c72be9abb25c000c70
rfinnegan <81394236+rfinnegan@users.noreply.github.com>
2022-03-09 14:59:33 -0500

    made menu sticky

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -11,12 +11,12 @@ const Navbar = () => {
     <div id="menu-container" class='menu-container'>
       <div class="menu-content desktop-only">
         <a style={padding} href="/home">Home</a>
-        <a style={padding} href="/contact">Contact Me</a>
+        <a style={padding} href="/contact">Contact</a>
       </div>
       <div class="mobile-only">
         <Menu pageWrapId={"app-content"} outerContainerId={"app-content"} right>
           <a className="menu-item" href="/home">Home</a>
-          <a className="menu-item" href="/contact">Contact Me</a>
+          <a className="menu-item" href="/contact">Contact</a>
         </Menu>
       </div>
     </div>
